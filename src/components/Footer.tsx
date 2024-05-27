7f81f8051b84ce28834bc69428f384179003d2e3
Fabio Correa Gomes <fabiocorreagomes@gmail.com>
2024-05-27 10:26:04 -0300

    showing Sabrina

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
     return (
         <div className=" bg-primary-orange text-center pt-2 pb-2">
             <p className="text-white">
-                Website developed by <span className=" font-bold underline">FabioCG.DEV</span>
+                Website developed by <span className=" font-bold underline">FabioCG.DEV</span> Teste Sabrina
             </p>
         </div>
     )   
